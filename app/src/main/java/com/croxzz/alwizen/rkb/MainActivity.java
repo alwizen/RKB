d851594d5ed588e72575e5bfd47a3bd719d5ed91
alwizen <alwizen@icloud.com>
2018-10-16 09:40:28 +0700

    Initial commit

---
+++ app/src/main/java/com/croxzz/alwizen/rkb/MainActivity.java
@@ -0,0 +1,100 @@
+package com.croxzz.alwizen.rkb;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_play;
+
+    MediaPlayer mediaPlayer;
+    boolean prepared = false;
+    boolean started = false;
+
+    String url_radio = "https://www.radioking.com/widgets/player/player.php?id=480&c=%23f39c12&ii=&p=1&pp=1&i=1&eq=1&po=1&t=1&a=1&f=big";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_play = (Button) findViewById(R.id.btn_play);
+        btn_play.setEnabled(false);
+        btn_play.setText("SILAHKAN TUNGGU");
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        new PlayerTask().execute(url_radio);
+
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (started){
+                    started = false;
+                    mediaPlayer.pause();
+                    btn_play.setText("PLAY");
+                }else {
+                    started = true;
+                    mediaPlayer.start();
+                    btn_play.setText("PAUSE");
+                }
+
+            }
+        });
+    }
+
+    class PlayerTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+
+            try {
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.prepare();
+                prepared = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            mediaPlayer.start();
+            btn_play.setEnabled(true);
+            btn_play.setText("PLAY");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (started){
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (started){
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (prepared){
+            mediaPlayer.release();
+        }
+    }
+}
